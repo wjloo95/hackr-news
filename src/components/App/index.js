30938f049fe0a66e679f72dc11f5c88c6d08e430
wjloo95 <loo.williamj@gmail.com>
2020-04-27 18:58:32 -0400

    add selectors ti app index

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,18 +1,21 @@
 import { connect } from 'react-redux';
 import actions from 'store/story/actions';
+import { hasMoreStoriesSelector } from 'store/story/selectors';
+
 import App from './App';
 
-const mapStateToProps = (state) => {
-  return {
-    stories: state.story.stories,
-    page: state.story.page,
-    storyIds: state.story.storyIds,
-    isFetching: state.story.isFetching,
-  };
-};
+const mapStateToProps = (state) => ({
+  stories: state.story.stories,
+  page: state.story.page,
+  storyIds: state.story.storyIds,
+  isFetching: state.story.isFetching,
+  hasMoreStories: hasMoreStoriesSelector(state),
+});
 
 const mapDispatchToProps = (dispatch) => ({
   fetchStoriesFirstPage: () => dispatch(actions.fetchStoryIDs()),
+  fetchStories: ({ storyIds, page }) =>
+    dispatch(actions.fetchStories({ storyIds, page })),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
