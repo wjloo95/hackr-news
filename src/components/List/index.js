828fb633809861e5a3474ece84224bb28cc87215
wjloo95 <loo.williamj@gmail.com>
2020-04-27 17:24:45 -0400

    create list component

---
+++ src/components/List/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ListItem from 'components/ListItem';
+import { ListWrapper } from './styles';
+
+const List = () => {
+  return (
+    <ListWrapper>
+      <ListItem />
+    </ListWrapper>
+  );
+};
+
+export default List;
