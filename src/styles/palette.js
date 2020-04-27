f92dbed4bda6fb6a35e56e2cb2a2489ac1452be2
wjloo95 <loo.williamj@gmail.com>
2020-04-27 17:05:33 -0400

    set up global styles

---
+++ src/styles/palette.js
@@ -0,0 +1,15 @@
+export const darkColors = {
+  background: '#272727',
+  backgroundSecondary: '#393C3E',
+  text: '#bfbebe',
+  textSecondary: '#848886',
+  border: '#272727',
+};
+
+export const lightColors = {
+  background: '#EAEAEA',
+  backgroundSecondary: '#F8F8F8',
+  text: '#848886',
+  textSecondary: '#aaaaaa',
+  border: '#EAEAEA',
+};
