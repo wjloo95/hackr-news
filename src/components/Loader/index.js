9386da340e91ebf49306e2c757ef464c1cb2d8a7
wjloo95 <loo.williamj@gmail.com>
2020-04-27 19:04:13 -0400

    add loader component

---
+++ src/components/Loader/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Animation } from './styles';
+
+const Loader = () => {
+  return (
+    <Animation>
+      <span>.</span>
+      <span>.</span>
+      <span>.</span>
+    </Animation>
+  );
+};
+
+export default Loader;
