5d8c4282f401383125d68e83d518a0dfa0d5e5ce
wjloo95 <loo.williamj@gmail.com>
2020-04-27 20:24:06 -0400

    set up layout in redux

--- src/store/app/actions.js
+++ src/store/app/actions.js
@@ -2,10 +2,12 @@ const NS = '@hnReader/app';
 
 export const actionTypes = {
   SET_THEME: `${NS}/SET_THEME`,
+  SET_LAYOUT: `${NS}/SET_LAYOUT`,
 };
 
 const actions = {
   setTheme: (payload = {}) => ({ type: actionTypes.SET_THEME, payload }),
+  setTheme: (payload = {}) => ({ type: actionTypes.SET_LAYOUT, payload }),
 };
 
 export default actions;
--- src/store/app/reducer.js
+++ src/store/app/reducer.js
@@ -1,10 +1,12 @@
 import { actionTypes } from './actions';
 
-const getInitialState = () => ({ theme: 'dark' });
+const getInitialState = () => ({ theme: 'dark', layout: 'list' });
 
 const app = (state = getInitialState(), { type, payload }) => {
   switch (type) {
     case actionTypes.SET_THEME:
+      return { ...state, ...payload };
+    case actionTypes.SET_LAYOUT:
       return { ...state, ...payload };
     default:
       return state;
