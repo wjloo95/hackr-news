2414f97fa9d1a872c63dc375bbd5f6e7161be789
wjloo95 <loo.williamj@gmail.com>
2020-04-27 21:21:23 -0400

    Updates

---
+++ precache-manifest.a41238c209bf27654b15bec45013bfb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43f476243866d38583ed4cfd50cc268f",
+    "url": "/hackr-news/index.html"
+  },
+  {
+    "revision": "ae43d8cdd0e8b9793815",
+    "url": "/hackr-news/static/js/2.e09635c4.chunk.js"
+  },
+  {
+    "revision": "741ce79e7197f3939479c7025151de8f",
+    "url": "/hackr-news/static/js/2.e09635c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0a47ae8eca32b368014",
+    "url": "/hackr-news/static/js/main.cb77d50d.chunk.js"
+  },
+  {
+    "revision": "e74b2a1c269581f349d7",
+    "url": "/hackr-news/static/js/runtime-main.298bff04.js"
+  }
+]);
